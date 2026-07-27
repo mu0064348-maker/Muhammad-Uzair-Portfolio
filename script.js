c442bce6f11aeb7f51b316357a6e2c8618bf9d73
mu0064348-maker <mu0064348@gmail.com>
2026-07-27 19:53:14 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+
+    console.log("Welcome to Muhammad Uzair's portfolio website");
+
+    let year = new Date().getFullYear();
+
+    document.querySelector("footer p").innerHTML =
+    "© " + year + " Muhammad Uzair | Learning and Creating";
+
+};
